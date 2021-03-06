090bc6c4e43d62013da488dd498597abd9b2991a
n-gras <niels.gras@outlook.com>
2021-03-06 19:07:05 +0100

    Frontend/react skeleton built

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
             services.AddAutoMapper(typeof(MappingProfiles));
         }
@@ -49,6 +56,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
