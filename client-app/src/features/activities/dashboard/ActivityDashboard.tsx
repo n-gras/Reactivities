db307b1bd5ac868798fb684f6df7b9b758e2231b
n-gras <niels.gras@outlook.com>
2021-06-14 18:53:51 +0200

    Added nicer styling to pages, added more details to ActivityDetails

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,30 +1,31 @@
-import { observer }         from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
-import { Grid }             from "semantic-ui-react";
-import LoadingComponent     from "../../../app/layout/LoadingComponent";
-import { useStore }         from "../../../app/stores/store";
+import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 
-import ActivityList         from "./ActivityList";
+import ActivityList from "./ActivityList";
 
-export default observer( function ActivityDashboard() {
-    
-    const {activityStore} = useStore();
-    const {loadActivities, activityRegistry} = activityStore;
+export default observer(function ActivityDashboard() {
+
+    const { activityStore } = useStore();
+    const { loadActivities, activityRegistry } = activityStore;
 
     useEffect(() => {
         if (activityRegistry.size <= 1) loadActivities();
     }, [activityRegistry.size, loadActivities])
 
     if (activityStore.loadingInitial) return <LoadingComponent content={'Loading app'} />
-    
+
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width={'6'}>
-                <h2>Activity filters</h2>
-            </Grid.Column>        
+                <ActivityFilters />
+            </Grid.Column>
         </Grid>
     )
 })
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,19 +1,20 @@
-import React                   from 'react';
-import ReactDOM                from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css';
-import { BrowserRouter }       from "react-router-dom";
-import App                     from './app/layout/App';
-import reportWebVitals         from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import App from './app/layout/App';
+import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
         <BrowserRouter>
-            <App /> 
+            <App />
         </BrowserRouter>
     </StoreContext.Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
