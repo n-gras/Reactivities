235b98f9f89adc43e0aa6a052cedcbf57ea16df7
n-gras <niels.gras@outlook.com>
2021-06-14 16:46:00 +0200

    Added routing to pages

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,34 @@
-import { observer }                   from "mobx-react-lite";
-import React, { useEffect } from 'react';
-import { Container }                  from 'semantic-ui-react';
-import ActivityDashboard              from "../../features/activities/dashboard/ActivityDashboard";
-import { useStore }                   from "../stores/store";
-import LoadingComponent               from "./LoadingComponent";
-import NavBar                         from "./NavBar";
+import { observer } from "mobx-react-lite";
+import React from 'react';
+import { Route, useLocation } from "react-router-dom";
+import { Container } from 'semantic-ui-react';
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import HomePage from "../../features/home/HomePage";
+import NavBar from "./NavBar";
 
 function App() {
-    const {activityStore} = useStore();
+  const location = useLocation();
 
-    useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore])
-    
-    if (activityStore.loadingInitial) return <LoadingComponent content={'Loading app'} />
-    
-    return (
-        <>
+  return (
+    <>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
             <NavBar />
-            <Container style={{marginTop: '7em'}}>
-                <ActivityDashboard />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
             </Container>
-
-        </>
-    );
+          </>
+        )}
+      />
+    </>
+  );
 }
 
 export default observer(App);
